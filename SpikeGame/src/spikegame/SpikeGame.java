98811ef961c26e66d38411855fdafbd85ad37646
Kathryn Hodge <kahodge@vassar.edu>
2014-09-26 12:34:46 -0400

    Project Creation!!

---
+++ SpikeGame/src/spikegame/SpikeGame.java
@@ -0,0 +1,25 @@
+
+package spikegame;
+
+/**
+ *
+ * @author kathrynhodge
+ */
+
+public class SpikeGame {
+
+    // Main Functions:
+    // test()
+    // draw()
+    // tick()
+    // react()
+    
+    // TIP: forever animation = while loop with true
+    
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
