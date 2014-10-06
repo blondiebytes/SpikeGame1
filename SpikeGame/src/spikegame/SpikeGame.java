785f7d714ef76e6f02f6a3fd0b835bd908a6fe52
Kathryn Hodge <kahodge@vassar.edu>
2014-10-05 20:37:51 -0400

    It's WORKING! :)

--- SpikeGame/src/spikegame/SpikeGame.java
+++ SpikeGame/src/spikegame/SpikeGame.java
@@ -1,101 +1,103 @@
 package spikegame;
+
 import net.slashie.libjcsi.wswing.WSwingConsoleInterface;
 import net.slashie.libjcsi.ConsoleSystemInterface;
 import net.slashie.libjcsi.CharKey;
 import java.util.ArrayList;
+import java.util.Iterator;
 
-// STILL NEED: How to Erase Bubble
+// TO DO :     DONE: How to Erase Bubble
 //             DONE: Game Over Function
-//             Bubble in range where spike can hit it
-//             Bubble data structure for all the bubbles (Queue? Array?)
+//             DONE: Bubble data structure for all the bubbles (ArrayList)
 //             DONE: Add label for score
 //             DONE: Add label for lives
-//             Get game to restart after the player looses 
-
+//             DONE: Get game to restart after the player looses 
+//             WAHOOOOOO. Now Testing. GAH
 
 public class SpikeGame {
+
     static int balloonTurnCount = 0;
-    static int currentBalloon = 0;
-    static int numberOfBalloonsOnScreen = 5;
     static boolean gameOver = false;
-    
-    public static void collision (Spike s, Balloon b, LivesLabel livesCount, ScoreLabel scoreCount) {
+    static int sentinalH = 55;
+
+    public static void collision(Spike s, Balloon b, LivesLabel livesCount, ScoreLabel scoreCount) {
         if ((s.width == b.width) && (s.height == b.height)) {
-          // Spike hits the bubble
+            // Spike hits the bubble
             livesCount.subtractLife();
             scoreCount.subtractScore();
-            b.height = 55;
-         //  System.out.println("I HAVE NOT COLLIDED");
+            b.height = sentinalH;
+            //  System.out.println("I HAVE NOT COLLIDED");
             if (livesCount.gameOver()) {
                 // Change gameOver to true;
                 gameOver = true;
             }
         }
-        
+
         if ((s.width != b.width) && (s.height == b.height)) {
             // spike misses the bubble
             scoreCount.addScore();
-            b.height = 55;
+            b.height = sentinalH;
         }
     }
-    
-    
-    public static void main ( String[] args ) {
-        
-        ConsoleSystemInterface s = new WSwingConsoleInterface("Spike by Kathryn", true);
-        
-        while (true) {
-        s.cls();
-        s.print(1, 0, "Press something to start the game!", s.GREEN);
-        s.refresh();
-        s.inkey();
-        
-        // Creating a new data 
-        ArrayList<Balloon> balloonDataStruct = new ArrayList();
-        gameOver = false;
-        
-        Balloon balloon = new Balloon();
-        balloonDataStruct.add(balloon);
-        Spike spike = new Spike();
-        LivesLabel livesLabel = new LivesLabel();
-        ScoreLabel scoreLabel = new ScoreLabel();
 
-        while (!gameOver) {
+    public static void main(String[] args) {
+
+        ConsoleSystemInterface s = new WSwingConsoleInterface("Spike by Kathryn", true);
+
+        while (true) {
             s.cls();
-            spike.draw(s);
-            for (Balloon b : balloonDataStruct) {
-              b.draw(s);
-              System.out.println("Drawing a balloon: " + balloonDataStruct.indexOf(b));
-            }
-            livesLabel.draw(s);
-            scoreLabel.draw(s);
-            balloonTurnCount++;
+            s.print(1, 0, "Press something to start the game!", s.GREEN);
             s.refresh();
-            CharKey k = s.inkey();
-            spike = spike.react(k);
-            // If the spike moves, balloons move
-            if (!spike.isEqualTo(spike.react(k))) {
+            s.inkey();
+
+            // Creating a new data 
+            ArrayList<Balloon> balloonDataStruct = new ArrayList();
+            gameOver = false;
+
+            Balloon balloon = new Balloon();
+            balloonDataStruct.add(balloon);
+            Spike spike = new Spike();
+            LivesLabel livesLabel = new LivesLabel();
+            ScoreLabel scoreLabel = new ScoreLabel();
+
+            while (!gameOver) {
+                s.cls();
+                spike.draw(s);
                 for (Balloon b : balloonDataStruct) {
-                      b = b.tick();
-                      }
+                    b.draw(s);
+                }
+                livesLabel.draw(s);
+                scoreLabel.draw(s);
+                balloonTurnCount++;
+                s.refresh();
+                CharKey k = s.inkey();
+                spike = spike.react(k);
+                // If the spike moves, balloons move
+                if (!spike.isEqualTo(spike.react(k))) {
+                    for (Balloon b : balloonDataStruct) {
+                        b = b.tick();
+                    }
+                }
+                // Adding a balloon every turns 
+                // This if may seem unneccessary, but it makes it easier if 
+                // I ever wanted to make the game easier
+                if (balloonTurnCount == 1) {
+                    balloonDataStruct.add(new Balloon());
+                    balloonTurnCount = 0;
+                }
+              
+                for (Iterator<Balloon> it = balloonDataStruct.iterator(); it.hasNext();) {
+                    Balloon b = it.next();
+                    collision(spike, b, livesLabel, scoreLabel);
+                    if (b.height == sentinalH) {
+                        it.remove();
+                    }
+                }
             }
-            if (balloonTurnCount == 3) {
-                balloonDataStruct.add(new Balloon());
-                balloonTurnCount = 0;
-            }
-            for (Balloon b : balloonDataStruct) {
-               collision(spike, b, livesLabel, scoreLabel);
-               if (b.height == 55) {
-                   Boolean bool = balloonDataStruct.remove(b);
-                   // it removes the balloon, but still crashing
-                   System.out.println("I removed a balloon! : " + bool);
-               }
-            }
-          }
-       s.cls(); 
-       s.print(1, 0, "GAME OVER! Press any key to start another game", s.RED);
-       s.refresh();
-       s.inkey();
-        }   
+            s.cls();
+            s.print(1, 0, "GAME OVER! Press any key to start another game!", s.RED);
+            s.refresh();
+            s.inkey();
+        }
     }
 }
